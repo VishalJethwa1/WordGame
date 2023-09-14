471ce6f3a46c41251528671ba5cc0317d31cc286
Vishal Jethwa <jethwa.vishal88@gmail.com>
2023-09-14 22:38:34 +0500

    added functionality;  code for attempts segment

--- wordgame.py
+++ wordgame.py
@@ -9,32 +9,35 @@ def main():
     global maxAttempts
     i = 1
     while i > 0: #this loop will continuously display select mode screen
-        print("Press 1 for Beginner Mode")
-        print("Press 2 for Moderate Mode")
-        print("Press 3 for Advance Mode")
+        try:
+            print("Select Level | 1 for Beginner | 2 for Moderate | 3 for Advance")
 
-        selectMode = int(input("Select Level"))
+            selectMode = int(input("Select Level"))
 
-        randomWord = None
-        maxAttempts = None
-
-        if selectMode == 1:
-            randomWord = random.choice(beginner)
-            maxAttempts = 3
-        elif selectMode == 2:
-            randomWord = random.choice(moderate)
-            maxAttempts = 4
-        elif selectMode == 3:
-            randomWord = random.choice(advance)
-            maxAttempts = 6
-        else:
             randomWord = None
+            maxAttempts = None
+
+            if selectMode == 1:
+                randomWord = random.choice(beginner)
+                maxAttempts = 3
+            elif selectMode == 2:
+                randomWord = random.choice(moderate)
+                maxAttempts = 4
+            elif selectMode == 3:
+                randomWord = random.choice(advance)
+                maxAttempts = 6
+            else:
+                randomWord = None
+        except:
+            print("Please select level by pressing numbers : 1 | 2 | 3")
+            continue
         
         j = 0
         while j < maxAttempts:
             print("CRACK A WORD")
             print(f"Level : {selectMode} \t Attempts : {maxAttempts}")
             userGuessedWord = input("Guess your word\n")
+            maxAttempts -= 1
             getWordFromList = randomWord
             print(getWordFromList)
             revealedWord = ""
@@ -48,16 +51,20 @@ def main():
                         revealedWord += " _ "
                     i+= 1
             except:
-                continue    
+                 continue    
             print(revealedWord) #test statement
             if revealedWord == userGuessedWord:
                 print("Congratulations! You cracked a word")
                 break
-
-            maxAttempts -= 1
+            else:
+                print("")
+              
         j+=i
+          
         if maxAttempts > 0:
             break
+    
+    
     i+=1
-
+    
 main()
